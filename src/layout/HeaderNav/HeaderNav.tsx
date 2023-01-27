ff8cc17adfa9abd9dd63c8e292175ed8c86d542e
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2023-01-26 21:30:26 -0500

    feat: implementes the first iteration of the dropdown that will change the font

--- src/layout/HeaderNav/HeaderNav.tsx
+++ src/layout/HeaderNav/HeaderNav.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-const HeaderNav = (props) => {
+import React from 'react';
+import icon from '../../assets/images/logo.svg';
+import Dropdown from '../../ui/Dropdown/Dropdown';
+
+interface HeaderNavInterface {
+  headerNav: {
+    logo: {
+      path: string;
+      altText: string;
+      title: string;
+      href: string;
+    };
+    fontDropdown: {
+      arrowIcon: string;
+      items: Array<object>;
+    };
+  };
+}
+
+const HeaderNav = (props: HeaderNavInterface) => {
+  const { headerNav } = props;
+  const { path, altText, title, href } = props.headerNav.logo;
   return (
-    <header className="appHeader">
-      <span class="material-symbols-outlined">menu_book</span>
+    <header className='appHeader'>
+      <a href={href} title={title}>
+        <img src={path} alt={altText} />
+      </a>
+
+      <Dropdown fontDropdown={headerNav.fontDropdown} />
     </header>
   );
 };

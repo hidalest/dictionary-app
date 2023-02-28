80f098bad2f701d702ebdf14db6b4cd1483d19af
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2023-02-27 19:26:20 -0500

    bugfix: created a specific footer component to fix css issues

--- src/layout/HeaderNav/HeaderNav.tsx
+++ src/layout/HeaderNav/HeaderNav.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import icon from '../../assets/images/logo.svg';
 import Dropdown from '../../ui/Dropdown/Dropdown';
 import Toggle from '../../ui/Toggle/Toggle';
 import './HeaderNav.scss';
